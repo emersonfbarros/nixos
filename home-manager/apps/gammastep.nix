212e98e6b6ec3de15e460ba09e8285aa02f84d05
Emerson Barros <emersonfb99@gmail.com>
2024-11-09 14:16:13 -0300

    feat(apps): adds gammastep

---
+++ home-manager/apps/gammastep.nix
@@ -0,0 +1,21 @@
+{
+  services.gammastep = {
+    enable = true;
+
+    temperature = {
+      day = 6500;
+      night = 2500;
+    };
+
+    dawnTime = "5:20-5:40";
+    duskTime = "17:46-18:06";
+
+    settings = {
+      general = {
+        fade = 1;
+        adjustment-method = "wayland";
+        gamma = 0.8;
+      };
+    };
+  };
+}
