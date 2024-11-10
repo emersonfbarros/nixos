695801fea475fdc03253e50b72dc0ee6fb8b98c0
Emerson Barros <emersonfb99@gmail.com>
2024-11-10 00:02:08 -0300

    feat(bin): adds `v` script, wrapper for nvim

---
+++ home-manager/bin/v.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+let
+  v = pkgs.writeShellScriptBin "v" ''
+    if [ -n "$1" ]; then
+      file=$1
+    else
+      file=$(selectfile)
+    fi
+
+    if [ -n "$file" ]; then
+      ${pkgs.neovim}/bin/nvim "$file"
+    fi
+      '';
+    in
+{
+  home.packages = [ v ];
+}
