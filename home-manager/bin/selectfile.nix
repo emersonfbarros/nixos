f749de0bacb7e8ba573b23444fe78777c544b031
Emerson Barros <emersonfb99@gmail.com>
2024-11-16 15:16:00 -0300

    chore(bin): improves `selectfile`

--- home-manager/bin/selectfile.nix
+++ home-manager/bin/selectfile.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
 let
   selectFile = pkgs.writeShellScriptBin "selectfile" ''
-    ${pkgs.fd}/bin/fd -tf -H --exclude .git --exclude node_modules | \
+    ${pkgs.fd}/bin/fd -tf -H \
+    --exclude ".git" \
+    --exclude "node_modules" \
+    --exclude "dist" \
+    --exclude "build" \
+    --exclude ".cache" \
+    --exclude "converage" | \
       ${pkgs.fzf}/bin/fzf --tmux 80% \
       --preview 'bat --color=always --style=header,grid --line-range :500 {}' \
       --preview-window=right,65%
