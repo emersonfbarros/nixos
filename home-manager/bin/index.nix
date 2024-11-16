91811cf6d6931d61b036ba835735f4933fde93f3
Emerson Barros <emersonfb99@gmail.com>
2024-11-16 15:14:52 -0300

    chore(bin): imports new scripts

--- home-manager/bin/index.nix
+++ home-manager/bin/index.nix
@@ -3,9 +3,14 @@
     ./chrb.nix
     ./chspk.nix
     ./chmic.nix
+    ./extract.nix
     ./tofi-power.nix
     ./zt.nix
     ./selectfile.nix
     ./v.nix
+    ./yt.nix
+    ./kitab.nix
+    ./kiwindow.nix
+    ./tofi-web-search.nix
   ];
 }
