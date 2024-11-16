0d2fec8937f3b5f6eb7d8014e9962b4bf2067716
Emerson Barros <emersonfb99@gmail.com>
2024-11-16 13:30:55 -0300

    refactor(system/locale): removes LC_IDENTIFICATION and LC_TIME

--- nixos/modules/locale.nix
+++ nixos/modules/locale.nix
@@ -4,14 +4,12 @@
   # Brazil mentioned
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
     LC_MEASUREMENT = "pt_BR.UTF-8";
     LC_MONETARY = "pt_BR.UTF-8";
     LC_NAME = "pt_BR.UTF-8";
     LC_NUMERIC = "pt_BR.UTF-8";
     LC_PAPER = "pt_BR.UTF-8";
     LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
   };
 
   # Configure keymap in X11
