e4070262962d2af27d6ba56952fe84d6f0d37a5f
Emerson Barros <emersonfb99@gmail.com>
2024-11-15 23:20:11 -0300

    feat(tofi): improves style

--- home-manager/apps/tofi.nix
+++ home-manager/apps/tofi.nix
@@ -1,23 +1,44 @@
+{ config, ... }:
 {
-  programs.tofi = {
-    enable = true;
-    settings = {
-      anchor = "top";
-      width = "100%";
-      height = 25;
-      horizontal = true;
+  programs.tofi =
+    let
+      colors = config.lib.stylix.colors;
+    in
+    {
+      enable = true;
+      settings = {
+        font = "JetBrainsMono NF";
+        font-size = 11;
 
-      outline-width = 0;
-      border-width = 0;
-      min-input-width = 150;
-      result-spacing = 15;
-      padding-top = 2;
-      padding-bottom = 0;
-      padding-left = 0;
-      padding-right = 0;
+        anchor = "top";
+        width = "100%";
+        height = 21;
+        horizontal = true;
 
-      prompt-text = ''" Run   "'';
-      fuzzy-match = true;
+        outline-width = 0;
+        border-width = 0;
+        min-input-width = 150;
+        result-spacing = 15;
+        padding-top = 2;
+        padding-bottom = 0;
+        padding-left = 0;
+        padding-right = 0;
+
+        prompt-text = ''" Run   "'';
+        fuzzy-match = true;
+
+        background-color = "#${colors.base00}";
+        text-color = "#${colors.base05}";
+
+        prompt-background = "#${colors.base02}";
+        prompt-background-padding = "1, 5, 1, 5";
+        prompt-color = "#${colors.base05}";
+
+        selection-background = "#${colors.base02}";
+        selection-background-padding = "1, 5, 1, 5";
+        selection-color = "#${colors.base05}";
+      };
     };
-  };
+
+  stylix.targets.tofi.enable = false;
 }
