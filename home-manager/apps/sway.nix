c1ac7319c0bedae3b37e42a3250b81763dc87f16
Emerson Barros <emersonfb99@gmail.com>
2024-11-16 13:37:00 -0300

    feat(sway): extra systemd config for screen sharing
    These lines loads important environment variables for the operation of several applications, especially xdg-desktop-portal-wlr. They are usually in `/etc/sway/config.d/*` in the usual Linux distributions, as this file does not exist I put them directly in the Sway configuration.

--- home-manager/apps/sway.nix
+++ home-manager/apps/sway.nix
@@ -151,7 +151,17 @@
     };
 
     extraConfig = ''
-      exec systemctl --user import-environment PATH && systemctl --user restart xdg-desktop-portal.service
+      exec systemctl --user set-environment XDG_CURRENT_DESKTOP=sway
+      exec systemctl --user import-environment DISPLAY \
+        SWAYSOCK \
+        WAYLAND_DISPLAY \
+        XDG_CURRENT_DESKTOP
+
+      exec hash dbus-update-activation-environment 2>/dev/null && \
+          dbus-update-activation-environment --systemd DISPLAY \
+              SWAYSOCK \
+              XDG_CURRENT_DESKTOP=sway \
+              WAYLAND_DISPLAY
     '';
 
     extraSessionCommands = ''
