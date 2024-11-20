bb29299510893217b5473298320cf8cbf559f501
Emerson Barros <emersonfb99@gmail.com>
2024-11-20 01:02:18 -0300

    refactor(firefox): user.js, bookmarks, and gnome theme
    - get betterfox dynamically fetching direct from github
    - adds bookmarks list
    - adds gnome ui theme

--- home-manager/apps/firefox.nix
+++ home-manager/apps/firefox.nix
@@ -1,256 +1,262 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   programs.firefox = {
     enable = true;
 
-    profiles.emerson = {
-      isDefault = true;
+    profiles.emerson =
+      let
+        gnomeTheme = pkgs.fetchFromGitHub {
+          owner = "rafaelmardojai";
+          repo = "firefox-gnome-theme";
+          rev = "v132";
+          sha256 = "sha256-lf9MQs8+NUvQd8b5t+7c4kLqUQixGO9WwWcLa1XYuiQ=";
+        };
+      in
+      {
+        isDefault = true;
 
-      containers = {
-        Personal = {
-          id = 1;
-          color = "blue";
-          icon = "fingerprint";
-        };
-        Professional = {
-          id = 2;
-          color = "green";
-          icon = "dollar";
-        };
-        Work = {
-          id = 3;
-          color = "yellow";
-          icon = "briefcase";
-        };
-      };
+        userChrome = ''
+          @import "${gnomeTheme}/userChrome.css";
+        '';
 
-      search = {
-        default = "DuckDuckGo";
-        privateDefault = "Google";
-        order = [
-          "DuckDuckGo"
-          "Google"
+        userContent = ''
+          @import "${gnomeTheme}/userContent.css";
+        '';
+
+        containers = {
+          Personal = {
+            id = 1;
+            color = "blue";
+            icon = "fingerprint";
+          };
+          Professional = {
+            id = 2;
+            color = "green";
+            icon = "dollar";
+          };
+          Work = {
+            id = 3;
+            color = "yellow";
+            icon = "briefcase";
+          };
+        };
+
+        search = {
+          default = "DuckDuckGo";
+          privateDefault = "Google";
+          order = [
+            "DuckDuckGo"
+            "Google"
+          ];
+        };
+
+        extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+          bitwarden
+          ublock-origin
+          youtube-shorts-block
+          privacy-badger
+          translate-web-pages
         ];
+
+        bookmarks = [
+          {
+            name = "Utils";
+            bookmarks = [
+              {
+                name = "Sheets";
+                url = "https://docs.google.com/spreadsheets/u/0/";
+              }
+              {
+                name = "Translate";
+                url = "https://translate.google.com/?sl=pt&tl=en&op=translate";
+              }
+              {
+                name = "GitHub";
+                url = "https://github.com";
+              }
+              {
+                name = "Slack";
+                url = "https://slack.com";
+              }
+              {
+                name = "Jira";
+                url = "https://klever-io.atlassian.net/jira/software/c/projects/KLC/boards/78";
+              }
+            ];
+          }
+          {
+            name = "Google";
+            bookmarks = [
+              {
+                name = "Gmail";
+                url = "https://mail.google.com/mail/u/0/";
+              }
+              {
+                name = "Drive";
+                url = "https://drive.google.com/drive/my-drive";
+              }
+              {
+                name = "Calendar";
+                url = "https://calendar.google.com/calendar/u/0/r/week";
+              }
+              {
+                name = "Maps";
+                url = "https://www.google.com/maps?authuser=0";
+              }
+              {
+                name = "Google";
+                url = "https://google.com";
+              }
+            ];
+          }
+          {
+            name = "Social";
+            bookmarks = [
+              {
+                name = "WhatsApp";
+                url = "https://web.whatsapp.com";
+              }
+              {
+                name = "Telegram";
+                url = "https://web.telegram.org/z";
+              }
+              {
+                name = "Reddit";
+                url = "https://reddit.com";
+              }
+              {
+                name = "Linkedin";
+                url = "https://linkedin.com";
+              }
+              {
+                name = "Discord";
+                url = "https://discord.com/channels/@me";
+              }
+              {
+                name = "Instagram";
+                url = "https://instagram.com";
+              }
+              {
+                name = "Twitter";
+                url = "https://twitter.com";
+              }
+            ];
+          }
+          {
+            name = "News";
+            bookmarks = [
+              {
+                name = "BBC Brasil";
+                url = "https://bbc.com/portuguese";
+              }
+              {
+                name = "Nexo";
+                url = "https://nexojornal.com.br";
+              }
+              {
+                name = "UOL Notícias";
+                url = "https://noticias.uol.com.br";
+              }
+              {
+                name = "New York Times";
+                url = "https://nytimes.com";
+              }
+              {
+                name = "BBC International";
+                url = "https://bbc.com";
+              }
+              {
+                name = "CNN";
+                url = "https://edition.cnn.com/";
+              }
+              {
+                name = "O Globo";
+                url = "https://oglobo.globo.com";
+              }
+              {
+                name = "Folha de São Paulo";
+                url = "https://folha.uol.com.br";
+              }
+              {
+                name = "G1";
+                url = "https://g1.globo.com/";
+              }
+              {
+                name = "G1 Alagoas";
+                url = "https://g1.globo.com/al/alagoas/";
+              }
+              {
+                name = "Piauí";
+                url = "https://piaui.folha.uol.com.br/";
+              }
+            ];
+          }
+          {
+            name = "Media";
+            bookmarks = [
+              {
+                name = "Genius";
+                url = "https://genius.com";
+              }
+              {
+                name = "Spotify";
+                url = "https://spotify.com";
+              }
+              {
+                name = "Youtube";
+                url = "https://youtube.com";
+              }
+              {
+                name = "Freedium";
+                url = "https://freedium.cfd/";
+              }
+            ];
+          }
+        ];
+
+        extraConfig =
+          let
+            betterFoxUserJs = builtins.readFile (
+              builtins.fetchurl {
+                url = "https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js";
+                sha256 = "sha256:1ghh8hpkh7q5q5bss4yd0xf7gakw6zh9ips5rc3g25sb6rsmdfij";
+              }
+            );
+
+            gnomeThemeUserJs = builtins.readFile "${gnomeTheme}/configuration/user.js";
+          in
+          ''
+            ${betterFoxUserJs}
+
+            // Overrides
+            user_pref("identity.fxaccounts.enabled", false); // Disable account syncing
+
+            // Smoothness on scrolling
+            // Sharpen Scrolling
+            user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+            user_pref("general.smoothScroll", true); // DEFAULT
+            user_pref("mousewheel.min_line_scroll_amount", 10); // 10-40; adjust this number to your liking; default=5
+            user_pref("general.smoothScroll.mouseWheel.durationMinMS", 80); // default=50
+            user_pref("general.smoothScroll.currentVelocityWeighting", "0.15"); // default=.25
+            user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6"); // default=.4
+
+            // OPTION: INSTANT SCROLLING (SIMPLE ADJUSTMENT)
+            // recommended for 60hz+ displays
+            user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+            user_pref("general.smoothScroll", true); // DEFAULT
+            user_pref("mousewheel.default.delta_multiplier_y", 275); // 250-400; adjust this number to your liking
+
+            // UI options
+            user_pref("browser.startup.page", 3); // Restore session on startup
+            user_pref("privacy.userContext.enabled", true); // Enable tabs containers
+            user_pref("privacy.userContext.newTabContainerOnLeftClick.enabled", true);
+
+            user_pref("browser.download.useDownloadDir", false); // Always ask you where to save files
+
+            ${gnomeThemeUserJs}
+
+            // user_pref("browser.uidensity", 1); // UI compat density
+          '';
       };
-
-      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-        bitwarden
-        ublock-origin
-        youtube-shorts-block
-        privacy-badger
-        translate-web-pages
-      ];
-
-      settings = {
-        # Restore session on startup
-        "browser.startup.page" = 3;
-
-        # Credits to Betterfox (https://github.com/yokoffing/Betterfox), I just put in nix syntax
-        # GENERAL
-        "content.notify.interval" = 100000;
-
-        # GFX
-        "gfx.canvas.accelerated.cache-items" = 4096;
-        "gfx.canvas.accelerated.cache-size" = 512;
-        "gfx.content.skia-font-cache-size" = 20;
-        # DISK CACHE
-        "browser.cache.jsbc_compression_level" = 3;
-
-        # MEDIA CACHE
-        "media.memory_cache_max_size" = 65536;
-        "media.cache_readahead_limit" = 7200;
-        "media.cache_resume_threshold" = 3600;
-
-        # IMAGE CACHE
-        "image.mem.decode_bytes_at_a_time" = 32768;
-
-        # NETWORK
-        "network.http.max-connections" = 1800;
-        "network.http.max-persistent-connections-per-server" = 10;
-        "network.http.max-urgent-start-excessive-connections-per-host" = 5;
-        "network.http.pacing.requests.enabled" = false;
-        "network.dnsCacheExpiration" = 3600;
-        "network.ssl_tokens_cache_capacity" = 10240;
-
-        # SPECULATIVE LOADING
-        "network.dns.disablePrefetch" = true;
-        "network.dns.disablePrefetchFromHTTPS" = true;
-        "network.prefetch-next" = false;
-        "network.predictor.enabled" = false;
-        "network.predictor.enable-prefetch" = false;
-
-        # EXPERIMENTAL
-        "layout.css.grid-template-masonry-value.enabled" = true;
-        "dom.enable_web_task_scheduling" = true;
-
-        ### SECTION: SECUREFOX ###
-
-        # TRACKING PROTECTION
-        "browser.contentblocking.category" = "strict";
-        "urlclassifier.trackingSkipURLs" = "*.reddit.com, *.twitter.com, *.x.com, *.twimg.com, *.tiktok.com";
-        "urlclassifier.features.socialtracking.skipURLs" = "*.instagram.com, *.twitter.com, *.x.com, *.twimg.com";
-        "browser.download.start_downloads_in_tmp_dir" = true;
-        "browser.helperApps.deleteTempFileOnExit" = true;
-        "browser.uitour.enabled" = false;
-        "privacy.globalprivacycontrol.enabled" = true;
-
-        # OCSP & CERTS / HPKP
-        "security.OCSP.enabled" = 0;
-        "security.remote_settings.crlite_filters.enabled" = true;
-        "security.pki.crlite_mode" = 2;
-
-        # SSL / TLS
-        "security.ssl.treat_unsafe_negotiation_as_broken" = true;
-        "browser.xul.error_pages.expert_bad_cert" = true;
-        "security.tls.enable_0rtt_data" = false;
-
-        # DISK AVOIDANCE
-        "browser.privatebrowsing.forceMediaMemoryCache" = true;
-        "browser.sessionstore.interval" = 60000;
-
-        # SHUTDOWN & SANITIZING
-        "privacy.history.custom" = true;
-
-        # SEARCH / URL BAR
-        "browser.urlbar.trimHttps" = true;
-        "browser.urlbar.untrimOnUserInteraction.featureGate" = true;
-        "browser.search.separatePrivateDefault.ui.enabled" = true;
-        "browser.urlbar.update2.engineAliasRefresh" = true;
-        "browser.search.suggest.enabled" = false;
-        "browser.urlbar.quicksuggest.enabled" = false;
-        "browser.urlbar.groupLabels.enabled" = false;
-        "browser.formfill.enable" = false;
-        "security.insecure_connection_text.enabled" = true;
-        "security.insecure_connection_text.pbmode.enabled" = true;
-        "network.IDN_show_punycode" = true;
-
-        # HTTPS-FIRST POLICY
-        "dom.security.https_first" = true;
-
-        # PASSWORDS
-        "signon.formlessCapture.enabled" = false;
-        "signon.privateBrowsingCapture.enabled" = false;
-        "network.auth.subresource-http-auth-allow" = 1;
-        "editor.truncate_user_pastes" = false;
-
-        # MIXED CONTENT + CROSS-SITE
-        "security.mixed_content.block_display_content" = true;
-        "pdfjs.enableScripting" = false;
-
-        # EXTENSIONS
-        "extensions.enabledScopes" = 5;
-
-        # HEADERS / REFERERS
-        "network.http.referer.XOriginTrimmingPolicy" = 2;
-
-        # CONTAINERS
-        "privacy.userContext.ui.enabled" = true;
-
-        # SAFE BROWSING
-        "browser.safebrowsing.downloads.remote.enabled" = false;
-
-        # MOZILLA
-        "permissions.default.desktop-notification" = 2;
-        "permissions.default.geo" = 2;
-        "permissions.manager.defaultsUrl" = "";
-        "webchannel.allowObject.urlWhitelist" = "";
-
-        # TELEMETRY
-        "datareporting.policy.dataSubmissionEnabled" = false;
-        "datareporting.healthreport.uploadEnabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.server" = "data:,";
-        "toolkit.telemetry.archive.enabled" = false;
-        "toolkit.telemetry.newProfilePing.enabled" = false;
-        "toolkit.telemetry.shutdownPingSender.enabled" = false;
-        "toolkit.telemetry.updatePing.enabled" = false;
-        "toolkit.telemetry.bhrPing.enabled" = false;
-        "toolkit.telemetry.firstShutdownPing.enabled" = false;
-        "toolkit.telemetry.coverage.opt-out" = true;
-        "toolkit.coverage.opt-out" = true;
-        "toolkit.coverage.endpoint.base" = "";
-        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-        "browser.newtabpage.activity-stream.telemetry" = false;
-
-        # EXPERIMENTS
-        "app.shield.optoutstudies.enabled" = false;
-        "app.normandy.enabled" = false;
-        "app.normandy.api_url" = "";
-
-        # CRASH REPORTS
-        "breakpad.reportURL" = "";
-        "browser.tabs.crashReporting.sendReport" = false;
-        "browser.crashReports.unsubmittedCheck.autoSubmit2" = false;
-
-        # DETECTION
-        "captivedetect.canonicalURL" = "";
-        "network.captive-portal-service.enabled" = false;
-        "network.connectivity-service.enabled" = false;
-
-        ### SECTION: PESKYFOX ###
-
-        # MOZILLA UI
-        "browser.privatebrowsing.vpnpromourl" = "";
-        "extensions.getAddons.showPane" = false;
-        "extensions.htmlaboutaddons.recommendations.enabled" = false;
-        "browser.discovery.enabled" = false;
-        "browser.shell.checkDefaultBrowser" = false;
-        "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
-        "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
-        "browser.preferences.moreFromMozilla" = false;
-        "browser.aboutConfig.showWarning" = false;
-        "browser.aboutwelcome.enabled" = false;
-        "browser.profiles.enabled" = true;
-
-        # THEME ADJUSTMENTS
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "browser.compactmode.show" = true;
-        "browser.privateWindowSeparation.enabled" = false;
-        "browser.newtabpage.activity-stream.newtabWallpapers.v2.enabled" = true;
-
-        # COOKIE BANNER HANDLING
-        "cookiebanners.service.mode" = 1;
-        "cookiebanners.service.mode.privateBrowsing" = 1;
-
-        # FULLSCREEN NOTICE
-        "full-screen-api.transition-duration.enter" = "0 0";
-        "full-screen-api.transition-duration.leave" = "0 0";
-        "full-screen-api.warning.timeout" = 0;
-
-        # URL BAR
-        "browser.urlbar.suggest.calculator" = true;
-        "browser.urlbar.unitConversion.enabled" = true;
-        "browser.urlbar.trending.featureGate" = false;
-
-        # NEW TAB PAGE
-        "browser.newtabpage.activity-stream.feeds.topsites" = false;
-        "browser.newtabpage.activity-stream.showWeather" = false;
-        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-
-        # POCKET
-        "extensions.pocket.enabled" = false;
-
-        # DOWNLOADS
-        "browser.download.manager.addToRecentDocs" = false;
-
-        # PDF
-        "browser.download.open_pdf_attachments_inline" = true;
-
-        # TAB BEHAVIOR
-        "browser.bookmarks.openInTabClosesMenu" = false;
-        "browser.menu.showViewImageInfo" = true;
-        "findbar.highlightAll" = true;
-        "layout.word_select.eat_space_to_next_word" = false;
-
-        ### SMOOTHFOX ###
-        "apz.overscroll.enabled" = true;
-        "general.smoothScroll" = true;
-        "mousewheel.default.delta_multiplier_y" = 275;
-
-        # Disable account syncing
-        "identity.fxaccounts.enabled" = false;
-      };
-    };
   };
 }
