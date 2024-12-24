9432f17d8b258e410a69bef587db93c3b641397b
Emerson Barros <emersonfb99@gmail.com>
2024-12-24 16:58:27 -0300

    refactor(user): uses variables defined on `flake.nix`

--- nixos/modules/user.nix
+++ nixos/modules/user.nix
@@ -1,21 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, user, ... }:
 {
   programs.zsh.enable = true;
 
   users = {
     defaultUserShell = pkgs.zsh;
 
-    users.emerson = {
+    users.${user} = {
       isNormalUser = true;
-      description = "emerson";
       extraGroups = [
         "networkmanager"
         "wheel"
       ];
-      packages = [ ];
     };
   };
 
   # doesn't work on home-manager :/
-  services.twingate.enable = true;
+  environment.systemPackages = [ pkgs.twingate ];
 }
