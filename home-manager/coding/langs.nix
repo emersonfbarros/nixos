a4b461f0c9c9f2153ea54192bf18aae92f03e3ea
Emerson Barros <emersonfb99@gmail.com>
2025-02-15 10:41:07 -0300

    feat(langs): adds GOPATH

--- home-manager/coding/langs.nix
+++ home-manager/coding/langs.nix
@@ -3,12 +3,16 @@
 {
   # Add base development tools to your environment
   home.packages = with pkgs; [
-    go
-
     rustc
     cargo
 
     nodejs
     python3
   ];
+
+  programs.go = rec {
+    enable = true;
+    goPath = "go";
+    goBin = "${goPath}/bin";
+  };
 }
