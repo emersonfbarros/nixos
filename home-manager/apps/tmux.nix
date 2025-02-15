d811358df3bacc5d39c48ba4918c8c746322e85e
Emerson Barros <emersonfb99@gmail.com>
2025-02-15 10:39:33 -0300

    feat(tmux): updates TERM and supports to under curl

--- home-manager/apps/tmux.nix
+++ home-manager/apps/tmux.nix
@@ -24,8 +24,12 @@ in
     escapeTime = 0;
     historyLimit = 100000;
     mouse = true;
-    terminal = "$TERM";
+    terminal = "tmux-256color";
     extraConfig = ''
+    set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
+    set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+
+
       # status bar style
       set -g status-position top
       set -g status-right-length 80
