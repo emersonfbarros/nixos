a951e6411c678d9a31fae9882fb9be9d8932d49c
Emerson Barros <emersonfb99@gmail.com>
2026-02-22 21:09:40 -0300

    remove ipv6 upstream dns servers from pihole module

--- modules/nixos/pihole.nix
+++ modules/nixos/pihole.nix
@@ -12,17 +12,10 @@
             "--ip=192.168.0.53"
             "--cap-add=SYS_TIME"
             "--cap-add=SYS_NICE"
-            # Explicitly force the container kernel to accept IPv6 Router Advertisements (SLAAC)
-            "--sysctl=net.ipv6.conf.all.accept_ra=2"
-            "--sysctl=net.ipv6.conf.all.autoconf=1"
-            "--sysctl=net.ipv6.conf.default.accept_ra=2"
-            "--sysctl=net.ipv6.conf.default.autoconf=1"
-            "--sysctl=net.ipv6.conf.eth0.accept_ra=2"
-            "--sysctl=net.ipv6.conf.eth0.autoconf=1"
           ];
           environment = {
             TZ = "America/Maceio";
-            FTLCONF_dns_upstreams = "1.1.1.1;8.8.8.8;2606:4700:4700::1111;2001:4860:4860::8888";
+            FTLCONF_dns_upstreams = "1.1.1.1;8.8.8.8";
             FTLCONF_dns_listeningMode = "ALL";
             FTLCONF_webserver_port = "80";
           };
@@ -44,7 +37,7 @@
         path = [ pkgs.podman ];
         script = ''
           if ! podman network exists pihole_macvlan; then
-            podman network create -d macvlan --ipv6 -o parent=enp2s0 --subnet=192.168.0.0/24 --gateway=192.168.0.1 pihole_macvlan
+            podman network create -d macvlan -o parent=enp2s0 --subnet=192.168.0.0/24 --gateway=192.168.0.1 pihole_macvlan
           fi
         '';
       };
